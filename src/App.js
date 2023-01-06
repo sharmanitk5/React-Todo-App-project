3663ce36caee342492f90e4892e3815ba385d822
sachin <sharmanitk5@gmail.com>
2023-01-06 12:12:23 -0500

    Todo App Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import logo from './logo.svg';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+
+
+  const[text,setText]=useState("");
+  const[todo,setTodo]=useState([]);
+
+
+  // useEffect(()=>{
+  //  if(todo==="")
+  //   setTodo([""])
+   
+  // },[]);
+   function AddData(e){
+          e.preventDefault();
+          if(text=="") return;
+       
+  setTodo([...todo,{text:text,id:uuidv4(),}]);
+   setText("");
+         
+       
+   };
+  const  DeleteData= (id)=>{
+
+ 
+       setTodo([...todo].filter((todo)=>todo.id!==id)
+
+    ) };
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={AddData}>
+        <input className="input" value = {text} onChange={e=>setText(e.target.value)} />
+        <button className="button" onClick={AddData}>Add</button>
+      </form>
+      
+
+        
+        
+          {todo.map((todo)=>(<div key={todo.id} className="Todo">
+        <p>{todo.text}</p>
+     
+            <i className="far fa-trash-alt" onClick={()=>DeleteData(todo.id)}></i>
+            </div>)
+
+           )}
+        
+         
+          
+           
+      
+      
+    
+
+        
+       
+      
+
+    
+   
+    </div>
     </div>
   );
 }
